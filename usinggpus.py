6f064b0657179c2287c58b173c3cfd335ddd9e61
Ceci Cohen <clc169@class-login.priv.cwru.edu>
2024-12-10 23:44:10 -0500

    add mpi communication for pipelining part 2

--- usinggpus.py
+++ usinggpus.py
@@ -21,14 +21,26 @@ import globals
 
 def main():
 
+### PARSE ARG OPTIONS ###
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--batch_size", default=4, type=int, help="specify batch size")
+    parser.add_argument("-e", "--epochs", default=2, type=int, help="specify number of training epochs")
+    parser.add_argument("-v", "--verbose", action="store_true", help="specify text output")
+    args = parser.parse_args()
+
+    num_workers = 1
+    batch_size  = args.batch_size
+    epochs      = args.epochs
+
+
+##### PART 0: GET MPI SETTINGS/PARAMETERS #####
     comm = MPI.COMM_WORLD
 
     #get 'dimensions' of world
     world_size = comm.Get_size()
     my_rank = comm.Get_rank()
 
-
-    #TEST IF MPI IS WORKING PROPERLY
+    #test if MPI is working properly
     if my_rank == 0:
         print(f"world size is: {world_size}")
     
@@ -36,79 +48,92 @@ def main():
 
 
 
-    ### SET UP DATA ###
-    transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+##### PART 1: INITIAL SETUP OF DATA AND MODEL #####
 
-    #compute size of mini-batches
-    mini_batch_size = max(1, batch_size//world_size)
+    ### DATA SETUP FOR WORLD NODE ###
+    if(my_rank == 0):
+        transform = tv.transforms.Compose([tv.transforms.ToTensor()])
 
-    #initialize and format data
-    training_data = tv.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    training_loader = tu.data.DataLoader(training_data, batch_size=mini_batch_size, shuffle=True, num_workers=num_workers)
+        #compute size of mini-batches
+        mini_batch_size = max(1, batch_size// (world_size - 1))
 
-    for i, batch in enumerate(training_loader): 
-        if i == 4: 
-            break
-        print(f"Batch {i+1}:") 
-        print(batch)
+        if batch_size% (world_size - 1):
+            print(f"ERROR: batch size of {batch_size} is not divisible by number of nodes { (world_size - 1)}. Defaulting to batch size {mini_batch_size*(world_size - 1)} ")
 
+        #initialize and format data
+        training_data = tv.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+        training_loader = tu.data.DataLoader(training_data, batch_size=mini_batch_size, shuffle=True, num_workers=num_workers)
 
-    ### MODEL AND PIPELINING SETUP ###
-    
-    #choose your model
-    model = models.resnet50()
-
-    # modify last layer to match 10 classes in CIFAR-10
-    model.fc = nn.Linear(model.fc.in_features, 10)
-
-    layers = list(model.children())
-
-    #calculate subsection size (number of layers per GPU)
-    subsection_size = (len(layers) + (world_size - 1))// world_size
-
-    #assign subsection of layers based on rank
-    if(my_rank > 0):
-       subsection = layers[(my_rank - 1)*subsection_size : min(len(layers), (my_rank)*subsection_size)]
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(params = model.parameters(), lr = 0.001)
-
-
-    ### TRAINING ###
-    if my_rank == 0:
         #determine batches that will be passed through
+        num_mini_batches = len(training_loader)
 
-        batches = training_data.divide
+    ### MODEL AND PIPELINING SETUP FOR PIPELINING NODES ###
+    else:
+        #choose your model (resnet50)
+        model = models.resnet50()
+
+        # modify last layer to match 10 classes in CIFAR-10
+        model.fc = nn.Linear(model.fc.in_features, 10)
+
+        layers = list(model.children())
+
+        #calculate subsection size (number of layers per GPU)
+        subsection_size = (len(layers) + (world_size - 1))// world_size
+
+        #assign subsection of layers based on rank
+        my_subsection = layers[(my_rank - 1)*subsection_size : min(len(layers), (my_rank)*subsection_size)]
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(params = model.parameters(), lr = 0.001)
+
+
+##### PART 2: TRAINING #####
+    if my_rank == 0:
+        #broadcast how many rounds of batches each node will have to process
+        comm.broadcast(num_mini_batches, root=0)
 
         #send batches to rank 1
-        for batch in batches:
-            comm.send(result, dest = (my_rank + 1), tag = (my_rank + 1))
+        for batch in range(0, num_mini_batches + world_size):
+            
+            if batch < num_mini_batches:
+                comm.send(training_loader[batch], dest = (my_rank + 1), tag = (my_rank + 1))
+
+            if batch > world_size:
+                #TODO: What to do with output?
+                comm.recv(output, dest = (my_rank + 1), tag = (my_rank + 1))
 
 
         #wait to recieve that number of batches back
 
 
-    elif my_rank > 0:
-        #recieve number of batches to accept
-        num_of_batches = comm.recv(source = 0, tag = 'numbatches')
+    elif my_rank > 0 and my_rank < (mpi_world - 1):
+        #recieve number of batches that will be passing through nodes
+        num_mini_batches = comm.broadcast(num_mini_batches, root=0)
 
-        for _ in range(num_of_batches):
-            comm.recv(source=(my_rank - 1), tag= my_rank)
-            result = model(data)
+        #pass each mini-batch through assigned layers
+        for _ in range(num_mini_batches):
+            data = comm.recv(source=(my_rank - 1), tag= my_rank)
+            result = my_subsection(data)
+            comm.send(result, dest = (my_rank + 1), tag = (my_rank + 1))
+    else:
+
+        for _ in range(num_mini_batches):
+            data = comm.recv(source=(my_rank - 1), tag= my_rank)
+            result = my_subsection(data)
+            comm.sendbuf(result, dest = (my_rank + 1), tag = (my_rank + 1))
+
+            
 
 #TODO: confirm that this properly sets up destination so that last GPU sends to world
-            comm.send(result, dest = (my_rank + 1)%world_size, tag = (my_rank + 1)%world_size)
+            
+#TODO: Should the last GPU send data to a buffer? 
 
 
-'''
-    ### BACKPROPOGATION ###
+
+##### PART 3: BACKPROPOGATION #####
     if my_rank == 0:
         #feed batches back into 
 
-    #device = ("cuda" if torch.cuda.is_available else "cpu")
-    #print("The device you are using is: ", device)
-    #model.to(device = device)
-    '''
 
     ''' start_time = time.time
     for epoch in range(epochs):
