1546da25e7092791b327839d69061a50b8981cc8
Ceci Cohen <clc169@class-login.priv.cwru.edu>
2024-12-11 23:40:04 -0500

    final updates

--- pipeline.py
+++ pipeline.py
@@ -40,9 +40,11 @@ def main():
     #epochs      = args.epochs
 
     num_workers = 1
-    batch_size  = 8
+    batch_size  = 32
+    mini_batch_size = 4
     epochs      = args.epochs
     debug = True
+
 
 
 ##### PART 0: GET MPI SETTINGS/PARAMETERS #####
@@ -59,24 +61,55 @@ def main():
 
 ##### PART 1: INITIAL SETUP OF DATA AND MODEL #####
 
+    device = ("cuda" if torch.cuda.is_available else "cpu")
+    print("The device you are using is: ", device) if debug else None
+
+
     ### DATA SETUP FOR WORLD NODE ###
     if(my_rank == 0):
         transform = tv.transforms.Compose([tv.transforms.ToTensor()])
 
-        #compute size of mini-batches
-        mini_batch_size = max(1, batch_size// (world_size - 1))
-
-        if batch_size% (world_size - 1):
-            print(f"ERROR: batch size of {batch_size} is not divisible by number of nodes { (world_size - 1)}. Defaulting to batch size {mini_batch_size*(world_size - 1)} ")
-
         #initialize and format data
         training_data = tv.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-        training_loader = tu.data.DataLoader(training_data, batch_size=mini_batch_size, shuffle=True, num_workers=num_workers)
+        training_loader = tu.data.DataLoader(training_data, batch_size=batch_size, shuffle=True,num_workers=num_workers)
+        data_iter = iter(training_loader)
+
+        #broadcast how many rounds of batches each node will have to process
+        #compute size of mini-batches
+        num_mini_batches = batch_size//mini_batch_size
+        num_batches = len(training_loader)
+
+        if batch_size%mini_batch_size != 0: 
+            print(f"error! batch size {batch_size} is not divisible by mini batch size {mini_batch_size}")
+        
+        print(f"num of mini batches: {num_mini_batches}") if debug else None
+        
+        comm.bcast(num_mini_batches, root=0)
+        comm.bcast(num_batches, root=0)
+
+        model = models.resnet50()
+
+        for param in model.parameters():
+            param.requires_grad = True
+
+        optimizer = optim.Adam(params = model.parameters(), lr = 0.001)
+        criterion = nn.CrossEntropyLoss()
+
 
     ### MODEL AND PIPELINING SETUP FOR PIPELINING NODES ###
     else:
+        #recieve num_mini_batches
+        num_mini_batches = 0
+        num_batches = 0
+        print(num_mini_batches)
+        num_mini_batches = comm.bcast(num_mini_batches, root=0)
+        num_batches = comm.bcast(num_batches, root=0)
+
+
         #choose your model (resnet50)
         model = models.resnet50()
+
+        optimizer = optim.Adam(params = model.parameters(), lr = 0.001)
 
         # modify last layer to match 10 classes in CIFAR-10
         model.fc = nn.Linear(model.fc.in_features, 10)
@@ -89,162 +122,110 @@ def main():
         #assign subsection of layers based on rank
         subsection_layers = layers[(my_rank - 1) * subsection_size : min(len(layers), my_rank * subsection_size)]
         my_subsection = nn.Sequential(*subsection_layers)
+        
+        for param in my_subsection.parameters():
+            param.requires_grad = True
 
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(params = model.parameters(), lr = 0.001)
 
 ##### PART 2: TRAINING #####
-    if my_rank == 0:
-        print(f"training {my_rank}!") if debug else None
+    for i in range(num_batches):
+        #loading!
+        loading(i, my_rank) if debug else None
 
-        #broadcast how many rounds of batches each node will have to process
-        num_mini_batches = len(training_loader)
-        print(f"batches: {num_mini_batches}") if debug else None
-        
-        comm.bcast(num_mini_batches, root=0)
-        
-        device = ("cuda" if torch.cuda.is_available else "cpu")
-        print("The device you are using is: ", device) if debug else None
+        ## FORWARD PASS ##
+        if my_rank == 0:
+            print(f"training {my_rank}!") if debug else None
+            #initialize losses array
+            losses = []
 
+            #split training data into inputs and labels
+            batch_inputs, batch_labels = next(data_iter)
 
-        #send batches to rank 1
-        for i, batch in enumerate(iterable = training_loader, start = 0):
+            #send batches to rank 1
+            for j in range(0, num_mini_batches):
 
-            #loading!
-            loading(i, my_rank) if debug else None
+                #send input to first GPU in pipeline
+                inputs = batch_inputs[j*mini_batch_size:j*mini_batch_size+mini_batch_size]
+                inputs = inputs.to(device)
+                comm.send(inputs, dest=(my_rank + 1), tag=(my_rank + 1)) 
+                #once enough data has been sent, start recieving model output
+            comm.Barrier()
+            result =comm.recv(source=(world_size - 1), tag= my_rank)
+            print(result)
+            for j, r in enumerate(result):
 
-            #send input to first GPU in pipeline
-            inputs, labels = batch
-            inputs, labels = inputs.to(device), labels.to(device)
-
-
-            if i % 200 == 0:
-                print("IN")
-                print(f"labels: {labels}")
-                print(f"inputs:{inputs}")
-                print()
-
-
-
-            comm.send(batch, dest=(my_rank + 1), tag=(my_rank + 1)) #TODO: do I really need tags?
-
-            #once enough data has been sent, start recieving model output
-            if i >= world_size:
-                #TODO: Figure out what to do with the loss
-                loss = comm.recv(source=(world_size - 1), tag=my_rank)
-                # Save the received loss periodically
-                #torch.save(loss, f'loss_checkpoint_batch_{batch}.pt')
-        
-        print("done with forward pass!")
-
-    elif my_rank > 0 and my_rank < (world_size - 1):
-        print(f"training {my_rank}!") if debug else None
-
-        #recieve number of batches that will be passing through nodes
-        num_mini_batches = None
-        num_mini_batches = comm.bcast(num_mini_batches, root=0)
-
-        #pass each mini-batch through assigned layers
-        for i in range(num_mini_batches):
-
-            #loading!
-            loading(i, my_rank) if debug else None
+                labels = batch_labels[j*mini_batch_size:j*mini_batch_size+mini_batch_size]
+                print('e')
+                print(r)
+                print(j)
+                loss = criterion(r.squeeze(), labels)
+                losses.append(loss)
 
 
-            #send and recieve model data
-            data = comm.recv(source=(my_rank - 1), tag= my_rank)
-            result = my_subsection(data[0]).squeeze()
+            print("done with forward pass!")
+        elif my_rank > 0 and my_rank < (world_size - 1):
+            print(f"training {my_rank}!") if debug else None
 
+            #pass each mini-batch through assigned layers
+            for i in range(num_mini_batches):
 
-            if i % 200 == 0:
-                print("MID")
-                print(f"labels: {data[1]}")
-                print(f"outputs:{result}")
-                print()
+                #send and recieve model data
+                data = comm.recv(source=(my_rank - 1), tag= my_rank)
+                data = data.to(next(my_subsection.parameters()).device)
+                result = my_subsection(data)
 
-            comm.send([result, data[1]], dest = (my_rank + 1), tag = (my_rank + 1))
-    else:
-        num_mini_batches = None
-        num_mini_batches = comm.bcast(num_mini_batches, root=0)
+                comm.send(result, dest = (my_rank + 1), tag = (my_rank + 1))
+            comm.Barrier()
 
-        for i in range(num_mini_batches):
+        else:
+            results = []
             
-            #loading!
-            loading(i, my_rank) if debug else None
+            print("eeee")
+            for i in range(num_mini_batches):
 
-            data = comm.recv(source=(my_rank - 1), tag= my_rank)
-            result = my_subsection(data[0]).squeeze()
-            labels = data[1]
+                data = comm.recv(source=(my_rank - 1), tag= my_rank)
+                result = my_subsection(data)
+                results.append(result)
+            comm.Barrier()
 
-            #compute loss in final layer
-            if i % 200 == 0:
-                print("OUT")
-                print(f"labels: {labels}")
-                print(f"outputs:{result}")
-                print()
+            comm.send(results, dest = 0, tag = 0)
+            print("SENT")
+        
+        
 
-            loss = criterion(result, labels)
-            comm.send(loss, dest = 0, tag = 0)
-
-
-
-
-##### PART 3: BACKPROPOGATION #####
-    if my_rank == 0:
-        if len(losses) != num_mini_batches:
-            print(f"Error: mismatch between num losses ({losses}) and num batches({batches})")
-
-        for i, loss in enumerate(losses):
-
+    ## BACKWARDS PASS ##
+        if my_rank == 0:
             #collect the final straggler outputs before starting backpropogation (after all inputs have been fed through)
-            if i < world_size:
-                loss = comm.recv(source=(world_size - 1), tag=my_rank)
-            
-            comm.send(loss, dest = (world_size - 1), tag=(world_size-1))
+            print("started")
+            for _ in range(num_mini_batches):
+                
+                #initialize gradient and pass through model
+                grad_output = torch.ones_like(losses[0])
+                optimizer.zero_grad()
+                losses[0].backward(grad_output, retain_graph=True)
+                comm.send(grad_output, dest=world_size - 1, tag=world_size - 1)
 
-    elif my_rank > 0 and my_rank < (world_size - 1):
+        elif my_rank > 0 and my_rank < (world_size - 1):
+            print("started")
 
-        for i in range(num_mini_batches):
+            for _ in range(num_mini_batches):
 
-            #loading!
-            loading(i, my_rank) if debug else None
+                #compute gradient based on previous input
+                prev_grad = comm.recv(source=my_rank + 1, tag=my_rank + 1)
+                result_grad = torch.autograd.grad(outputs=my_subsection.output, inputs=my_subsection.parameters(), grad_outputs=prev_grad)
+                comm.send(result_grad, dest=my_rank - 1, tag=my_rank - 1)
 
-            #send and recieve model data
-            data = comm.recv(source=(my_rank + 1), tag=my_rank)
-            result = backwards(data)
-            comm.send(result, dest =(my_rank - 1), tag =(my_rank - 1))
+        else:
+            for _ in range(num_mini_batches):
 
+                #compute gradient based on previous input
+                loss_grad = comm.recv(source=0, tag=my_rank)
+                loss_grad.backward()
+                comm.send(loss_grad, dest=my_rank - 1, tag=my_rank - 1)
 
-    else:
-
-        for i in range(num_mini_batches):
-            
-            #loading!
-            loading(i, my_rank) if debug else None
-
-            loss = comm.recv(source=0, tag= my_rank)
-            loss.backwards(data)
-
-            #compute loss in final layer
-            outputs, labels = result
-            gradient = criterion(outputs, labels)
-            comm.send(gradient, dest =(my_rank - 1), tag =(my_rank - 1))
+        print(f"done with backwards pass!")
 
 
-
-''' 
-            # update weights
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-
-            if i % 20  == 19:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss : {running_loss / 20:.3f}')
-
-    end_time = time.time
-
-'''
     
 
 if __name__ == "__main__": 
