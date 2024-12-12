e3869208708411869e46c28871a2b978790e6a41
ac8xy <170558845+ac8xy@users.noreply.github.com>
2024-12-12 02:53:58 -0500

    Update baseline_lightning.py. It works

--- baseline_lightning.py
+++ baseline_lightning.py
@@ -1,120 +1,176 @@
 #!/usr/bin/env python3
+#
+# Code modified from PA3-template.py provided by Sanmukh Kuppannagar for CSDS 451
+# updated for homework to use lightning
+# changed to accommodate the pytorch resnet50 model
 
+import os
 import time
+import argparse
 import torch
+#from torch.utils.data import Dataset, DataLoader, random_split
 import torch.nn as nn
-import torch.optim as optim
 import torch.utils as tu
+import torchvision as tv
+import torch.optim as optim
+import pytorch_lightning as L # convention... not very pythonic
+
 import torchvision as tv
 import torchvision.models as models
 
-import pytorch_lightning as pl
-from pytorch_lightning.strategies import DDPStrategy
-from torch.utils.data import DataLoader
 
-class CIFAR10Data(pl.LightningDataModule):
-    def __init__(self, batch_size=128):
-        super().__init__()
-        self.batch_size = batch_size
 
-    def prepare_data(self):
-        # Download data if needed
-        tv.datasets.CIFAR10(root="data", train=True, download=True)
+class L_Net(L.LightningModule):
+    """ Class to utilize the same kind of operations as before, but with
+    Pytorch Lightning.
+    
+    Based on tutorial https://lightning.ai/docs/pytorch/stable/starter/converting.html
 
-    def setup(self, stage=None):
-        # Transformations
-        transform = tv.transforms.Compose([
-            tv.transforms.ToTensor(),
-            tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+    Basically, it adds some kind of encoder
+    """
+    def __init__(self, encoder=None, batch_size=8):
         
-        if stage == 'fit' or stage is None:
-            self.cifar10_train = tv.datasets.CIFAR10(root="data", train=True, transform=transform)
-
-    def train_dataloader(self):
-        return DataLoader(self.cifar10_train, batch_size=self.batch_size, shuffle=True, num_workers=4)
-
-class ResNet50Pipeline(pl.LightningModule):
-    def __init__(self, num_classes=10, lr=0.01):
+        #super(L_Net, self).__init__()
         super().__init__()
-        self.lr = lr
 
-        #get resnet model
-        resnet50 = models.resnet50(pretrained=False)
-        resnet50.fc = nn.Linear(resnet50.fc.in_features, num_classes)
+        # based on the dev tutorial recomendations
+        # here https://lightning.ai/docs/pytorch/stable/advanced/transfer_learning.html
+        # for resnet50 applied to imagenet, start with default weights
+        # their example was applied to CIFAR-10, but labled for imagenet... so...
+        # that's not a red flag I hope
+        self.backbone = models.resnet50(weights="DEFAULT")
+        num_filters = self.backbone.fc.in_features
+        layers = list(self.backbone.children())[:-1]
+        self.feature_extractor = nn.Sequential(*layers)
+        self.feature_extractor.eval()
+        num_target_classes = 10
+        self.classifier = nn.Linear(num_filters, num_target_classes)
 
-        #get number of available gpus
-        num_gpus = torch.cuda.device_count()
-
-        #divide model into sublayers based on the number of GPUs
-        layers = list(resnet50.children())
-        subsection_size = len(layers) // num_gpus
-        subsection = [nn.Sequential(*layers[i * subsection_size:(i + 1) * subsection_size]) for i in range(num_gpus)]
-
-        #wrap stages into Pipe
-        self.pipeline = torch.distributed.pipeline.sync.Pipe(
-            nn.Sequential(*subsection),
-            chunks=2,
-            devices=[f"cuda:{i}" for i in range(num_gpus)],
-            checkpoint="never"
-        )
+        self.batch_size = batch_size
+        self.encoder = encoder
+        self.automatic_optimization = False
 
     def forward(self, x):
-        return self.pipeline(x)
+        return self.backbone(x)
+
+    def get_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
     def training_step(self, batch, batch_idx):
+        '''Method to run training step
+        '''
+        # previously in loop
+        inputs, labels = batch
 
-        #time forwards pass
-        start_time = time.time()
-        x, y = batch
-        outputs = self.forward(x)
-        forward_time = time.time() - start_time
+        # still not sure if I need an encoder at all
+        # parts of the tutorial make it seem non-optional...
+        if self.encoder is not None:
+            encoded_inputs = self.encoder(inputs)
+        else:
+            encoded_inputs = inputs
+        
+        # pulled from the loop
+        outputs = self(encoded_inputs)
 
-        loss = nn.CrossEntropyLoss()(outputs, y)
+        # okay, this didn't turn up any running processes
+        #os.system("nvidia-smi")
 
-        #time backwards pass
-        start_time = time.time()
+        # pulled this from the loop as well
+        loss = nn.functional.cross_entropy(outputs, labels)
+
+        # code stack exchange said to add to avoid error
         self.manual_backward(loss)
-        backward_time = time.time() - start_time
+        optimizer = self.optimizers()
+        scheduler = self.lr_schedulers()
+        optimizer.step()
+        optimizer.zero_grad()
+        scheduler.step()
 
-        self.log("train_loss", loss)
-        self.log("forward_time", forward_time, prog_bar=True)
-        self.log("backward_time", backward_time, prog_bar=True)
 
         return loss
 
+    def manual_backward(self, loss):
+        loss.backward()
+
+    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx,
+                       second_order_closure, on_tpu, using_native_amp,
+                       using_lbfgs):
+        optimizer.step()
+        
+
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
 
-if __name__ == "__main__":
+        # every tutorial has a different optimizer configuration and it's honestly
+        # very frustrating
+
+         # return optimizer
+        return [optimizer], [lr_scheduler]
+
+class CIFARDataModule(L.LightningDataModule):
+
+    def __init__(self, batch_size=100):
+        self.batch_size=batch_size
+
+    # more methods because the lightning intro video was so compelling
+    def prepare_data(self):
+        
+        transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+        training_data   = tv.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+
+    def setup(self, stage):
+        # do we need a transformer? this was in the pytorch tutorial
+        transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+
+        # You should implement these for CIFAR-10. HINT: The dataset may be accessed with Torchvision.
+        self.training_data = tv.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+
+    def train_dataloader(self):
+        training_loader = tu.data.DataLoader(self.training_data, batch_size=self.batch_size) 
+        return training_loader
+
+def main():
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--batch_size", default=4, type=int, help="specify batch size")
-    parser.add_argument("-e", "--epochs", default=2, type=int, help="specify number of training epochs")
-    parser.add_argument("-g", "--gpus", default=3, type=int, help="specify number of gpus")
+    parser.add_argument("-b", "--batch_size", default=8, type=int, help="specify batch size")
+    parser.add_argument("-e", "--epochs", default=1, type=int, help="specify number of training epochs")
     parser.add_argument("-v", "--verbose", action="store_true", help="specify text output")
-
+    parser.add_argument("--devices", default=None)
     args = parser.parse_args()
 
-    num_workers = 1
     batch_size  = args.batch_size
     epochs      = args.epochs
-    gpus        = args.gpus
+    device      = args.devices
+
+    transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+    training_data   = tv.datasets.CIFAR10(root='./data', train=True,
+                                          download=True, transform=transform)
+    training_loader = tu.data.DataLoader(training_data, batch_size=batch_size,
+                                         shuffle=True, num_workers=1)
+
+    #training_data = CIFARDataModule(batch_size=batch_size)
+    # import the pre-built pytorch module
+    # https://pytorch.org/hub/nvidia_deeplearningexamples_resnet50/
+
+    net = L_Net(batch_size=batch_size)
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if args.verbose: print("The device you are using is: ", device)
+    net.to(device = device)
+
+    trainer = L.Trainer(accelerator='gpu', devices=2, max_epochs=epochs)
+
+    start = time.time()
+    trainer.fit(net, training_loader, training_data)
+    end = time.time()
+    
+    # This is just here to let you know that your model has finished training.
+    print(f"Finished training. Used {epochs} epochs with {batch_size} samples per batch, which took {end-start} seconds.")
 
 
-    init_process_group("nccl")
-
-    #initalize data
-    data = CIFAR10Data(batch_size=128)
-
-    #initialize model
-    model = ResNet50Pipeline()
-
-    #train the model
-    trainer = pl.Trainer(
-        strategy=DDPStrategy(find_unused_parameters=False),
-        accelerator="gpu",
-        devices=gpus,
-        max_epochs=10
-    )
-    trainer.fit(model, datamodule=data)
+if __name__=="__main__":
+    main()
